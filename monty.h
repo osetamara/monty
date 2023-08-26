af953943da89552ae4c2eb704ef6ead42dc9cf9a
osetamara <osemudiamebrifini@gmail.com>
2023-08-26 05:15:30 -0700

    add

--- monty.h
+++ monty.h
@@ -75,5 +75,5 @@ void f_rotr(stack_t **head, __attribute__((unused)) unsigned int counter);
 void addnode(stack_t **head, int n);
 void addqueue(stack_t **head, int n);
 void f_queue(stack_t **head, unsigned int counter);
-
+void f_stack(stack_t **head, unsigned int counter);
 #endif 
